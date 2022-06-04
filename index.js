4bf21f0747d4ae14fce1daad1e1e68736acbb88a
Edem Essang <edemoski.eu@gmail.com>
2022-06-04 19:31:18 +0100

    full app

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-document.getElementById("count-el").innerText = 5
+
+
+// change from innerText to textContent 
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let welcomeEl = document.getElementById("welcome-el")
+let myName = "Edem Ukoh"
+let greeting = "welcome to the station"
+
+
+
+welcomeEl.textContent = greeting + "\n" + myName 
+
+
+
+
+console.log(countEl);
+
+let count = 0
+function increment(){
+    count  += 1
+    countEl.textContent = count
+}
+
+
+
+function save(){
+let countStr = count + " - " 
+saveEl.textContent += countStr
+countEl.textContent = 0
+count = 0
+}
+
